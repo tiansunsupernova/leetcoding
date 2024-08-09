8ff257fb4c5ab1946e239c555e772e1d0159bf72
InquisitiveMindHasToKnow <omarraymond18@gmail.com>
2024-08-09 00:13:45 -0400

    Adds folder for project euler questions. Adds main to run files. Adds multiples_of_three_or_five.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from omar.projecteuler.multiples_of_three_or_five import sum_of_multiples
+
+
+def main():
+    # Multiple of 3 or 5 question
+    limit_for_multiple_of_3_and_5 = 1000
+    sum_of_multiples(limit_for_multiple_of_3_and_5
+
+)
+
+
+if __name__ == "__main__":
+    main()
