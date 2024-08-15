8e2ef0a4d9f89bca33314f59646758c49a13373a
InquisitiveMindHasToKnow <omarraymond18@gmail.com>
2024-08-14 23:29:29 -0400

    Adds 10001st prime number question to projecteuler folder

---
+++ omar/projecteuler/ten_thousand_and_first_prime_num.py
@@ -0,0 +1,26 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13 , we can see that the 6th prime is 13.
+# What is the 10001st prime number?
+
+def is_prime_num(num):
+    if num <= 1:
+        return False
+    elif num == 2: # Account for 2, the only even prime number
+        return True
+    elif num % 2 == 0: # Not prime if even
+        return False
+    for i in range(3, int(num**0.5) + 1, 2): # Loop starts at 3, since 2 is covered, runs to
+                                             # square root of num and increments by 2 on each iteration
+        if num % i == 0: # if even return false, else true
+            return False
+    return True
+
+
+def find_10001st_prime():
+    count = 0 # count of prime numbers so sar
+    number = 1
+    while count < 10001: # Loop until 10001 prime numbers have been added to count
+        number += 1 # increment num to check
+        if is_prime_num(number):
+            count += 1
+    return number
+
